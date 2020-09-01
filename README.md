11028f818f08c2fff86eb5a0b5f8298e451121b8
RiffGuy <jins3569@gmail.com>
2020-09-01 23:47:33 +0900

    create python file

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # testing_YOLOv2
  Let's make a YOLOv2 model myself
+
+# Specification
+CPU: Intel(R) Core(TM) i5-6600 CPU @ 3.30GHz
+GPU: GeForce RTX 2060 SUPER
+OS: Ubuntu 16.04
+
+# Python Library
+-Tensorflow: 2.2.0
+
+# Using COCO dataset
---
+++ YOLOv2.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import numpy as np
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+class YOLOv2():
+    def __init__(self):
+        pass
